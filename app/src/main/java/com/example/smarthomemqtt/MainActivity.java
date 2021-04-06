7ab81015888d79abe8b4c528ceb144454ee9cd3a
hm-gruber13 <gruber13@hm.edu>
2021-04-06 08:42:57 +0200

    Ersten Prototyp der folgendes enthält: 1. Anmeldemaske (MainActivity) 2. Home-Bildschirm/Dashboard (HomeMainActivitiy) 3. Seite zum Hinzufügen von Devices (AddDeviceActivity)

--- app/src/main/java/com/example/smarthomemqtt/MainActivity.java
+++ app/src/main/java/com/example/smarthomemqtt/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.smarthomemqtt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText username = (EditText)findViewById(R.id.username);
+        EditText password = (EditText)findViewById(R.id.password);
+        Button login_button = (Button) findViewById(R.id.login_button);
+        TextView login_error_text = (TextView) findViewById(R.id.login_error_text);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Intent intent = new Intent(MainActivity.this, HomeMainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    login_error_text.setText("Wrong Credentials");
+                }
+            }
+        });
     }
 }
